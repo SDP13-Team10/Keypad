9b8866d5fffa0d82fe25197ee15f73ee80cdc952
Eric Moore <eric.o.moore@gmail.com>
2013-05-16 02:55:17 -0400

    Initial commit

---
+++ Keypad.py
@@ -0,0 +1,26 @@
+import serial
+
+class keypad:
+
+	kpad
+	BaudRate = 9600
+	keypadLocation = "/dev/ttyUSB0"
+
+	def __init__(self):
+		global kpad
+		
+		kpad = serial.Serial(keypadLocation,BaudRate)
+		
+		
+	def ReadLine():
+		global kpad
+		
+		while kpad.inWaiting() > 0:
+			buff += kpad.read()
+			
+		return buff
+		
+	def SendLine(data):
+		global kpad
+		
+		kpad.write(data)
